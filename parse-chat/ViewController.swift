198c60f8a4e7587cb93ae75587746a2466f8189e
Sherman Leung <shermankleung@gmail.com>
2015-04-23 18:39:29 -0700

    Initial Commit

---
+++ parse-chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parse-chat
+//
+//  Created by Sherman Leung on 4/23/15.
+//  Copyright (c) 2015 Sherman Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
